1b7963a554cea64c24ff3bf6b19cc43d4bc63f8e
U. Doug Hong <udoughong@hotmail.com>
2018-09-23 23:54:14 +0800

    Solution4DynamicAddTextBox

---
+++ Solution4DynamicAddTextBox/Action4AspNetWebForm/Demo.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Demo.aspx.cs" Inherits="Action4AspNetWebForm.Demo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Button1：動態新增一個textbox，ID='txt_1'" OnClick="Button1_Click" />
+            <br />
+            <asp:Button ID="Button2" runat="server" Text="Button2：讀取txt_1.text放到label" OnClick="Button2_Click" />
+            <asp:Panel ID="Panel1" runat="server">
+                <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            </asp:Panel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Solution4DynamicAddTextBox/Action4AspNetWebForm/Demo.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Action4AspNetWebForm
+{
+    public partial class Demo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (ViewState["Ctl_count"] != null)
+            {
+                TextBox txb = new TextBox();
+                txb.ID = "txt_1";
+                Panel1.Controls.Add(txb);
+                ViewState["Ctl_count"] = 1;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TextBox txb = new TextBox();
+            txb.ID = "txt_1";
+            Panel1.Controls.Add(txb);
+            ViewState["Ctl_count"] = 1;
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox txb = (TextBox)Panel1.FindControl("txt_1");
+            Label1.Text = txb.Text;
+        }
+    }
+}
